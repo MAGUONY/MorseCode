72a06db79a6cd71b1fb906afc2778b9d02ca18b1
Ronny Acevedo <58488936+MAGUONY@users.noreply.github.com>
2021-04-14 14:01:07 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+morseCodeAlphabet = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+}
+
+
+def textToMorseCode(text):
+    morseCode = ""
+
+    for letter in text:
+        morseCode += morseCodeAlphabet.get(letter) + " "
+
+    return morseCode
+
+
+print("Hello! This program converts any text to morse code!")
+userInput = input("Type in your text: ").upper()
+
+print(f"The morse code for that text is: {textToMorseCode(userInput)}")
+
